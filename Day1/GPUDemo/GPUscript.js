c5cb33a75e0c3ba2fa3085f184c56bf3b4832761
Weedy V2 <thanhduydinh@gmail.com>
2021-03-31 15:59:36 +0700

    commit Gpu Jquery

--- Day1/GPUDemo/GPUscript.js
+++ Day1/GPUDemo/GPUscript.js
@@ -1,18 +1,46 @@
 function chooseCard() {
     var select = document.getElementById("graphicCard").value;
-    //NVIDIA
-    var Nvidia = arrayNvidia = ["GTX 750", "GTX 1050", "GTX 1650"];
+    // //NVIDIA
+    // var Nvidia = arrayNvidia = ["GTX 750", "GTX 1050", "GTX 1650"];
 
-    //AMD
-    var Amd = arrayAmd = ["RX 550", "RX 570", "RX 600"];
+    // //AMD
+    // var Amd = arrayAmd = ["RX 550", "RX 570", "RX 600"];
 
 
     if (select === "Nvidia") {
         //Option Nvidia
         $("#gpuNvidia").css("display", "block");
+        document.getElementById("graphicCard").disabled = true;
+        // $("#manufacturerNvidia").css("display", "block");
 
-    } else {
+
+    } else if (select === "Amd") {
         //Option AMDs
         $("#gpuAmd").css("display", "block");
+        document.getElementById("graphicCard").disabled = true;
+        // $("#manufacturerAmd").css("display", "block");
     }
+}
+
+function chooseGPUSeries() {
+    document.getElementById("gpuSeriesNvidia");
+    $("#manufacturerNvidia").css("display", "block");
+}
+
+function chooseModelNvidia() {
+    var select = document.getElementById("selectManufacturerNvidia").value;
+    if (select === "Asus") {
+        $("#videoCardNvidia").css("display", "block");
+    } else {
+
+    }
+
+}
+
+function reChoice() {
+    document.getElementById("graphicCard").disabled = false;
+    $("#gpuNvidia").css("display", "none");
+    $("#gpuAmd").css("display", "none");
+    $("#manufacturerNvidia").css("display", "none");
+    $("#manufacturerAmd").css("display", "none");
 }
